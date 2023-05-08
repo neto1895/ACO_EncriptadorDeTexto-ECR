48c619f1af7cb8c2cf17530404c4ddefc4405f5a
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-05-08 16:47:15 -0600

    clean code and add tooltip

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,63 @@
 const textArea = document.querySelector(".ingresetexto");
 const mensaje = document.querySelector(".mensaje");
+let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
 
+function btnEncriptar(){
+    if(textArea.value === ""){
+        sintexto();
+    } else{
+        const textoEncriptado = encriptar(textArea.value);
+        displayMensaje(textoEncriptado);
+    }
+}
 
 function encriptar(stringEncriptado){
-    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
     stringEncriptado = stringEncriptado.toLowerCase();
-
     for( let i = 0; i < matrizCodigo.length; i++ ){
         if(stringEncriptado.includes(matrizCodigo[i][0])){
             stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]); 
-        }
-    }
+    }}
     return stringEncriptado;
 }
 
-function btnEncriptar(){
-    const textoEncriptado = encriptar(textArea.value);
+function btnDesencriptar(){
+    if(textArea.value === ""){
+        sintexto();
+    }else{
+    const textoEncriptado = desencriptar(textArea.value);
+    displayMensaje(textoEncriptado);
+    }
+}
+
+function desencriptar(stringDesencriptado){
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+    for( let i = 0; i < matrizCodigo.length; i++ ){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]); 
+    }}
+    return stringDesencriptado;
+}
+
+function copyText(){
+    mensaje.select();
+    mensaje.setSelectionRange(0,99999);
+    navigator.clipboard.writeText(mensaje.value);
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + mensaje.value;
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
+
+function sintexto(){
+    mensaje.style.display = "none";
+    document.querySelector(".btnCopiar").style.display = "none";
+    document.querySelector(".noText").style.display = "inherit";
+}
+
+function displayMensaje(textoEncriptado){
     mensaje.value = textoEncriptado;
     textArea.value = "";
     mensaje.style.display = "flex";
@@ -23,28 +65,9 @@ function btnEncriptar(){
     document.querySelector(".noText").style.display = "none";
 }
 
-function desencriptar(stringDesencriptado){
-    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    stringDesencriptado = stringDesencriptado.toLowerCase();
-
-    for( let i = 0; i < matrizCodigo.length; i++ ){
-        if(stringDesencriptado.includes(matrizCodigo[i][1])){
-            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]); 
-        }
-    }
-    return stringDesencriptado;
-}
-
-function btnDesencriptar(){
-    const textoEncriptado = desencriptar(textArea.value);
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
-}
-
 //toggle
 const checkbox = document.getElementById('checkbox');
-
 checkbox.addEventListener('change', ()=>{
-  document.body.classList.toggle('dark');
+document.body.classList.toggle('dark');
 });
 //toggle code ends
